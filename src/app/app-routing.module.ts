978e8984befb6dedae99dd337e6da6b1b164a582
Alexander Black <aeonrush@live.ru>
2019-06-17 19:03:39 +0300

    Add Universal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page', loadChildren: () => import('./pages/about/about.module').then((M) => M.AboutModule) },
+  { path: 'test', component: TestComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
